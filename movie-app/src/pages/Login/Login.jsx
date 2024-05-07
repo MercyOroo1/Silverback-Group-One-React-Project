299829ff00daa95418d5e4fa55b46ca57a298737
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-07 11:54:07 +0300

    adds login page
    Be sure to use vite when loading this page

--- movie-app/src/pages/Login/Login.jsx
+++ movie-app/src/pages/Login/Login.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
-  return (
-    <div className="login">Login
 
+   const [signState, setSignState] = useState("Sign In")
+
+  return (
+    <div className="login">
+       <img src={logo} className='login-logo' alt=""/>
+       <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState ==="Sign Up" ? <input type="text" placeholder="Name"/>:<></>}
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Passwords"/>
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox"/>
+              <label htmlFor=''>Remember Me</label>
+            </div>
+            <p>Need Some Help Nigga??</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In"?
+          <p>New to Netflix?<span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now!</span></p>
+          :<p>Already have an account?<span onClick={()=>{setSignState("Sign In")}}>Sign In</span></p>
+        } 
+        </div>
+       </div>
     </div>
   )
 }
