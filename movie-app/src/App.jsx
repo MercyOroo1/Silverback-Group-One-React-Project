4db1a495f3e32553dac5518ba74d3b77641a696a
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-05-08 09:17:12 +0300

    displaying data from an API

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect }, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
 import {Routes, Route, useNavigate} from 'react-router-dom'
 import Login from './pages/Login/Login'
@@ -6,28 +6,36 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Player from './pages/Player/Player'
 
 
-const App = () => {
+ const App = () => {
+
   const navigate = useNavigate();
 
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        console.log("Logged In!");
-        navigate('/');
-      } else {
-        console.log("Not Logged In!");
-        navigate('/login');
-      }
-    });
-  }, []);
+   useEffect(()=>{
+      onAuthStateChanged(auth, async(user)=>{
+        if(user){
+          console.log("Logged In!");
+          navigate('/')
+        }else{
+          console.log("Not Logged In!");
+          navigate('/login')
+        }
+      })
+   },[])
 
   return (
     <div>
-      <Home />
+      <ToastContainer theme="dark"/>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path = '/player' element= {<Player/>}/>
+      </Routes>
+      
       </div>
   )
 }
 
-export default App;
+export default App
